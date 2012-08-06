8a8b75fbfab497f508ab1306c28a17d19ef1d562
Michał Zieliński <michal@zielinscy.org.pl>
2012-08-06 15:51:04 +0200

    removed term/

--- term/pushtodevice.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in *.py; do
-    adb shell rm /sdcard/com.zielm.emacs.debug/${i}o
-    adb push $i /sdcard/com.zielm.emacs.debug/$i
-done
-
-adb push scripts /sdcard/com.zielm.emacs.debug/
